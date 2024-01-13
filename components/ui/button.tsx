8f7c30d15abf69e0f727326a93aafa63c5b5da9c
themuuln <zerone.offical@gmail.com>
2024-01-13 16:09:03 +0800

    loading prop added

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
+import { IoReloadOutline } from 'react-icons/io5';
 
 const buttonVariants = cva(
 	'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -35,11 +36,13 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 	loading?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, asChild = false, ...props }, ref) => {
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, asChild = false, loading = false, ...props }, ref) => {
 	const Comp = asChild ? Slot : 'button';
 	return (
 		<>
-			<Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+			<Comp disabled={loading} className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+				{loading ? <IoReloadOutline className={'animate-spin'} /> : ''}
+			</Comp>
 		</>
 	);
 });
